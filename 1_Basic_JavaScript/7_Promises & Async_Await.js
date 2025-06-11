6ddd0b5a04f3b36368d16ce89d1360786925988e
Ana Laf <zanlamar@gmail.com>
2025-06-11 18:33:45 +0200

    set part 7 ready

---
+++ 1_Basic_JavaScript/7_Promises & Async_Await.js
@@ -0,0 +1,34 @@
+// 1.7  PROMISES & ASYNC / AWAIT
+
+/* NIVEL 1
+
+    EJ 1: PROMISE CREATION
+    Creació d'una Promesa: Crea una promesa que es resolgui després de 2 segons i que retorni la cadena de text 'Hola, món'. */
+
+
+
+
+    /* EJ 2: PROMISE USE
+    Utilització d'una Promesa: Utilitza la promesa creada en l'exercici anterior. Crea un .then que imprimeixi el resultat a la consola. */
+
+
+
+    /* EJ 3: PROMISE WITH REJECT
+    Promesa amb reject: Crea una promesa que es resolgui després de 2 segons si l'input és igual a 'Hola', i que la rebutgi si l'input és qualsevol altra cosa. */
+
+
+
+    /* EJ 4: ASYNC / AWAIT
+    Ús de async/await: Escriu una funció asíncrona que utilitzi la funció await per a esperar el resultat de la promesa creada a l'exercici 1, i que després imprimeixi aquest resultat a la consola. */
+
+
+
+/* NIVEL 2
+
+    EJ 5: ERROR MANAGEMENT W SYNC / AWAIT
+    Gestió d'errors amb async/await: Modifica la funció de l'exercici 4 per a que capturi qualsevol possible error utilitzant un bloc try/catch. */
+
+
+    
+    /* EJ 6: PROMISE.ALL
+   Promise.all: Crea dues promeses que es resolguin després de 2 i 3 segons, respectivament. Utilitza Promise.all per a esperar que ambdues promeses es resolguin, i imprimeix els resultats a la consola. */
