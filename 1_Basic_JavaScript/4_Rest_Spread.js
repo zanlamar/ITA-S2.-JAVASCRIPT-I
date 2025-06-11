1dba1dc73293fcca688688181bcc3377a9bb580e
Ana Laf <zanlamar@gmail.com>
2025-06-11 13:22:29 +0200

    parte 4 WIP

--- 1_Basic_JavaScript/4_Rest_Spread.js
+++ 1_Basic_JavaScript/4_Rest_Spread.js
@@ -31,7 +31,10 @@
     EJ 3: COPY OBJECTS WITH SPREAD
     Copiant objectes amb Spread: Crea un objecte 'objecte1'. Després crea un segon objecte, 'objecte2', que sigui una còpia de 'objecte1' utilitzant l'operador spread. Canvia una propietat de 'objecte2' i comprova que 'objecte1' no ha canviat. */
 
-
+    const objecte1 = {
+        name: 'Limón';
+        raza: ''
+    }
 
 
 
@@ -51,9 +54,18 @@
 /* NIVEL 3
 
     EJ 5: SPREAD IN FUNCTIONS
-    Rest en Destructuring: Crea una array amb diversos elements. Utilitza destructuring i l'operador rest per a assignar els primers dos elements a variables, i després assignar la resta dels elements a una tercera variable. */
+    Spread en Funcions: Crea una funció que accepti tres arguments. Després, crea una array amb tres elements i crida la funció utilitzant l'operador spread amb aquesta array.*/
+
+    const functionSpread = (x, y, z) => x + y - z;
+
+    const lista = [100, 50, 1];
+
+    functionSpread(...lista);
+    console.log(functionSpread(...lista));
 
 
 
     /* EJ 6: FUSION OBJECT WITH SPREAD
     Fusionant Objectes amb Spread: Crea dos objectes amb propietats diferents. Utilitza l'operador spread per a fusionar aquests dos objectes en un de nou. */
+
+    
