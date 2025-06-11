3bcc3adef60bc7360383e68bbf380a65a0cbefa9
Ana Laf <zanlamar@gmail.com>
2025-06-11 13:01:49 +0200

    part 1.4 basics WIP

---
+++ 1_Basic_JavaScript/4_Rest_Spread.js
@@ -0,0 +1,59 @@
+// 1.4  REST & SPREAD OPERATORS
+
+/* NIVEL 1
+
+    EJ 1: SPREAD OPERATOR IN ARRAYS
+    Operador Spread en Arrays: Crea dues arrays, array1 i array2. Utilitza l'operador spread per a crear una tercera array que contingui tots els elements de array1 i array2. */
+
+    const arrayA = [4, 8, 15, 16, 23, 42];
+    const arrayB = [14, 28, 35, 46, 53, 62];
+
+    const arrayC = [...arrayA, ...arrayB];
+    console.log(arrayC);
+
+
+
+/*  EJ 2: REAST OPERATOR IN FUNCTIONS
+    Operador Rest en Funcions: Crea una funció 'suma' que utilitzi l'operador rest per a acceptar un nombre indeterminat d'arguments i retornar la seva suma. */
+
+    const suma = (...numbers) => {
+        let total = 0;
+        numbers.forEach(element => total += element);
+        return total;
+    };
+
+    const numbers = [4, 8, 15, 16, 23, 42];
+    console.log(suma(...numbers));
+
+
+/*  NIVEL 2
+
+    EJ 3: COPY OBJECTS WITH SPREAD
+    Copiant objectes amb Spread: Crea un objecte 'objecte1'. Després crea un segon objecte, 'objecte2', que sigui una còpia de 'objecte1' utilitzant l'operador spread. Canvia una propietat de 'objecte2' i comprova que 'objecte1' no ha canviat. */
+
+
+
+
+
+
+
+   /* EJ 4: REST IN DESTRUCTURING
+    Rest en Destructuring: Crea una array amb diversos elements. Utilitza destructuring i l'operador rest per a assignar els primers dos elements a variables, i després assignar la resta dels elements a una tercera variable. */
+
+    const arrayVariado = [ 1, 'a', null, 'Paco', [3, 4, 5]];
+    const [a, b, ...rest] = arrayVariado;
+
+    console.log(a);
+    console.log(b);
+    console.log(rest);
+
+
+/* NIVEL 3
+
+    EJ 5: SPREAD IN FUNCTIONS
+    Rest en Destructuring: Crea una array amb diversos elements. Utilitza destructuring i l'operador rest per a assignar els primers dos elements a variables, i després assignar la resta dels elements a una tercera variable. */
+
+
+
+    /* EJ 6: FUSION OBJECT WITH SPREAD
+    Fusionant Objectes amb Spread: Crea dos objectes amb propietats diferents. Utilitza l'operador spread per a fusionar aquests dos objectes en un de nou. */
