58d5c0efbce65bc397da458048227b76e7ef8f28
Ana Laf <zanlamar@gmail.com>
2025-06-12 11:02:32 +0200

    promises WIP

--- 1_Basic_JavaScript/7_Promises & Async_Await.js
+++ 1_Basic_JavaScript/7_Promises & Async_Await.js
@@ -5,16 +5,47 @@
     EJ 1: PROMISE CREATION
     Creació d'una Promesa: Crea una promesa que es resolgui després de 2 segons i que retorni la cadena de text 'Hola, món'. */
 
-
+    function greeting() {
+        return new Promise((resolve) => {
+            setTimeout(() => {
+                resolve(`Hola, món`);
+            }, 2000);
+        });
+    }
 
 
     /* EJ 2: PROMISE USE
     Utilització d'una Promesa: Utilitza la promesa creada en l'exercici anterior. Crea un .then que imprimeixi el resultat a la consola. */
 
+    greeting()
+        .then((result) => {
+            console.log(result);
+    });
+
 
 
     /* EJ 3: PROMISE WITH REJECT
     Promesa amb reject: Crea una promesa que es resolgui després de 2 segons si l'input és igual a 'Hola', i que la rebutgi si l'input és qualsevol altra cosa. */
+
+    function checkHi(input) {
+        return new Promise((resolve, reject) => {
+            setTimeout(() => {
+                if (input === 'Hola') {
+                    resolve('Hola a ti también');
+                } else {
+                    reject('¿No saludas?');
+                }
+            }, 2000);
+        });
+    };
+
+    checkHi('Bye')
+        .then((result) => {
+            console.log(`Todo ok.`, result);
+        })
+        .catch((error) => {
+            console.log(`Vaya...`, error);
+    });
 
 
 
@@ -29,6 +60,8 @@
     Gestió d'errors amb async/await: Modifica la funció de l'exercici 4 per a que capturi qualsevol possible error utilitzant un bloc try/catch. */
 
 
-    
-    /* EJ 6: PROMISE.ALL
+
+/*  NIVEL 3
+
+    EJ 6: PROMISE.ALL
    Promise.all: Crea dues promeses que es resolguin després de 2 i 3 segons, respectivament. Utilitza Promise.all per a esperar que ambdues promeses es resolguin, i imprimeix els resultats a la consola. */
